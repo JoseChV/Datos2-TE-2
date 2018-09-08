c091a7cce9018af548f17382d61bb24bbfcde18f
Brad0914 <bradmvf09081999@gmail.com>
2018-09-07 22:10:01 -0600

    Add files via upload
    Facade

---
+++ C++/Facade/cmake-build-debug/CMakeFiles/Facade.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Facade.dir/main.cpp.obj"
+  "Facade.pdb"
+  "Facade.exe"
+  "Facade.exe.manifest"
+  "libFacade.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Facade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
